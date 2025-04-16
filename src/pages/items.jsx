c63dc619674b31d86fe0ab884a72144895ce3d73
doudouchevalier <edouard.chevalier@student.junia.com>
2025-04-16 15:13:20 +0200

    Ajout des grilles de produits

--- src/pages/items.jsx
+++ src/pages/items.jsx
@@ -1,10 +1,12 @@
+import EPGrid10 from '../components/products_grid'; // ajuste le chemin si besoin
+
 function Items() {
-    return (
-      <div>
-        <h1>Itemsssss</h1>
-      </div>
-    )
-  }
-  
-  export default Items
-  
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-8">Itemsssss</h1>
+      <EPGrid10 />
+    </div>
+  );
+}
+
+export default Items;
